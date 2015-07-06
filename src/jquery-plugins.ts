9568451f149d1e2ac85bf3e73cd0d280304c78c8
edsilv <e.silverton@gmail.com>
2015-07-06 17:44:09 +0100

    added updateAttr

--- src/jquery-plugins.ts
+++ src/jquery-plugins.ts
@@ -9,49 +9,20 @@
         });
     };
 
-    $.fn.enable = function () {
-        return this.each(function () {
-            var $this = $(this);
-            $this.removeClass('disabled');
-            $this.attr('tabindex', $this.data('tabindex'));
-        });
-    };
+    $.fn.ellipsis = function (chars) {
 
-    $.fn.targetBlank = function () {
         return this.each(function () {
-            $(this).find('a').prop('target', '_blank');
-        });
-    };
 
-    $.fn.swapClass = function (removeClass, addClass) {
-        return this.each(function () {
-            $(this).removeClass(removeClass).addClass(addClass);
-        });
-    };
+            var $self = $(this);
 
-    $.fn.toggleClass = function (class1, class2) {
-        return this.each(function () {
-            var $this = $(this);
+            var text = $self.text();
 
-            if ($this.hasClass(class1)){
-                $(this).removeClass(class1).addClass(class2);
-            } else {
-                $(this).removeClass(class2).addClass(class1);
+            if (text.length > chars) {
+                var trimmedText = text.substr(0, chars);
+                trimmedText = trimmedText.substr(0, Math.min(trimmedText.length, trimmedText.lastIndexOf(" ")))
+
+                $self.empty().html(trimmedText + "&hellip;");
             }
-
-        });
-    };
-
-    $.fn.toggleText = function (text1, text2) {
-        return this.each(function () {
-            var $this = $(this);
-
-            if ($this.text() === text1){
-                $(this).text(text2);
-            } else {
-                $(this).text(text1);
-            }
-
         });
     };
 
@@ -142,137 +113,6 @@
 
     };
 
-    $.fn.toggleExpandText = function (chars, callback) {
-
-        return this.each(function () {
-
-            var $self = $(this);
-
-            var expandedText = $self.html();
-
-            if (chars > expandedText.length) return;
-
-            var expanded = false;
-
-            var collapsedText = expandedText.substr(0, chars);
-            collapsedText = collapsedText.substr(0, Math.min(collapsedText.length, collapsedText.lastIndexOf(" ")));
-
-            $self.toggle = function() {
-                $self.empty();
-
-                var $toggleButton = $('<a href="#" class="toggle"></a>');
-
-                if (expanded) {
-                    $self.html(expandedText + "&nbsp;");
-                    $toggleButton.text("less");
-                    $toggleButton.toggleClass("less", "more");
-                } else {
-                    $self.html(collapsedText + "&nbsp;");
-                    $toggleButton.text("more");
-                    $toggleButton.toggleClass("more", "less");
-                }
-
-                $toggleButton.one('click', function(e) {
-                    e.preventDefault();
-
-                    $self.toggle();
-                });
-
-                expanded = !expanded;
-
-                $self.append($toggleButton);
-
-                if (callback) callback();
-            };
-
-            $self.toggle();
-        });
-
-    };
-
-    $.fn.ellipsis = function (chars) {
-
-        return this.each(function () {
-
-            var $self = $(this);
-
-            var text = $self.text();
-
-            if (text.length > chars) {
-                var trimmedText = text.substr(0, chars);
-                trimmedText = trimmedText.substr(0, Math.min(trimmedText.length, trimmedText.lastIndexOf(" ")))
-
-                $self.empty().html(trimmedText + "&hellip;");
-            }
-        });
-
-    };
-
-    $.fn.equaliseHeight = function (reset, average) {
-
-        var maxHeight = -1;
-        var minHeight = Number.MAX_VALUE;
-        var heights = [];
-
-        // reset all heights to auto first so they can be re-measured.
-        if (reset){
-            this.each(function () {
-                $(this).height('auto');
-            });
-        }
-
-        this.each(function () {
-            var currentHeight = $(this).height();
-            heights.push(currentHeight);
-            maxHeight = maxHeight > currentHeight ? maxHeight : currentHeight;
-            minHeight = minHeight < currentHeight ? minHeight : currentHeight;
-        });
-
-        var finalHeight = maxHeight;
-
-        if (average) {
-            finalHeight = Math.median(heights);
-        }
-
-        this.each(function () {
-            $(this).height(finalHeight);
-        });
-
-        return this;
-    };
-
-    // Recursively removes the last empty element (img, audio, etc) or word in an element
-    $.fn.removeLastWord = function (chars) {
-
-        if ('undefined' === typeof chars) chars = 8;
-
-        return this.each(function () {
-
-            var $self = $(this);
-
-            if ($self.contents().length > 0) {
-                var $lastElement = $self.contents().last();
-                if ($lastElement[0].nodeType === 3) { // Text node
-                    var words = $lastElement.text().trim().split(' ');
-
-                    if (words.length > 1) {
-                        words.splice(words.length-1, 1);
-                        $lastElement[0].data = words.join(' '); // textnode.data
-                        return;
-                    } else if ('undefined' !== typeof chars && words.length === 1 && words[0].length > chars) {
-                        $lastElement[0].data = words.join(' ').substring(0, chars);
-                        return;
-                    }
-                }
-
-                $lastElement.removeLastWord(chars); // Element
-            } else {
-                // Empty element
-                $self.remove();
-            }
-        });
-    };
-
     // Truncates to a certain number of letters, while ignoring and preserving HTML
     $.fn.ellipsisHtmlFixed = function (chars, callback) {
 
@@ -309,6 +149,235 @@
                     $toggleButton.toggleClass("less", "more");
                 } else {
                     $self.html(collapsedText + "&hellip; ");
+                    $toggleButton.text("more");
+                    $toggleButton.toggleClass("more", "less");
+                }
+
+                $toggleButton.one('click', function(e) {
+                    e.preventDefault();
+
+                    $self.toggle();
+                });
+
+                expanded = !expanded;
+
+                $self.append($toggleButton);
+
+                if (callback) callback();
+            };
+
+            $self.toggle();
+        });
+    };
+
+    $.fn.enable = function () {
+        return this.each(function () {
+            var $this = $(this);
+            $this.removeClass('disabled');
+            $this.attr('tabindex', $this.data('tabindex'));
+        });
+    };
+
+    $.fn.equaliseHeight = function (reset, average) {
+
+        var maxHeight = -1;
+        var minHeight = Number.MAX_VALUE;
+        var heights = [];
+
+        // reset all heights to auto first so they can be re-measured.
+        if (reset){
+            this.each(function () {
+                $(this).height('auto');
+            });
+        }
+
+        this.each(function () {
+            var currentHeight = $(this).height();
+            heights.push(currentHeight);
+            maxHeight = maxHeight > currentHeight ? maxHeight : currentHeight;
+            minHeight = minHeight < currentHeight ? minHeight : currentHeight;
+        });
+
+        var finalHeight = maxHeight;
+
+        if (average) {
+            finalHeight = Math.median(heights);
+        }
+
+        this.each(function () {
+            $(this).height(finalHeight);
+        });
+
+        return this;
+    };
+
+    $.fn.horizontalMargins = function () {
+        var $self = $(this);
+        return parseInt($self.css('marginLeft')) + parseInt($self.css('marginRight'));
+    };
+
+    $.fn.horizontalPadding = function () {
+        var $self = $(this);
+        return parseInt($self.css('paddingLeft')) + parseInt($self.css('paddingRight'));
+    };
+
+    $.mlp = {x:0,y:0}; // Mouse Last Position
+    function documentHandler(){
+        var $current = this === document ? $(this) : $(this).contents();
+        $current.mousemove(function(e){jQuery.mlp = {x:e.pageX,y:e.pageY}});
+        $current.find("iframe").load(documentHandler);
+    }
+    $(documentHandler);
+    $.fn.ismouseover = function(overThis) {
+        var result = false;
+        this.eq(0).each(function() {
+            var $current = $(this).is("iframe") ? $(this).contents().find("body") : $(this);
+            var offset = $current.offset();
+            result =    offset.left<=$.mlp.x && offset.left + $current.outerWidth() > $.mlp.x &&
+            offset.top<=$.mlp.y && offset.top + $current.outerHeight() > $.mlp.y;
+        });
+        return result;
+    };
+
+    var on = $.fn.on, timer;
+    $.fn.on = function () {
+        var args = Array.apply(null, arguments);
+        var last = args[args.length - 1];
+
+        if (isNaN(last) || (last === 1 && args.pop())) return on.apply(this, args);
+
+        var delay = args.pop();
+        var fn = args.pop();
+
+        args.push(function () {
+            var self = this, params = arguments;
+            clearTimeout(timer);
+            timer = setTimeout(function () {
+                fn.apply(self, params);
+            }, delay);
+        });
+
+        return on.apply(this, args);
+    };
+
+    $.fn.onEnter = function (callback) {
+
+        return this.each(function() {
+
+            var $this = $(this);
+
+            $this.on('keyup', function(e) {
+                if (e.keyCode === 13) {
+                    e.preventDefault();
+                    callback();
+                }
+            });
+        });
+    };
+
+    $.fn.onPressed = function (callback) {
+
+        return this.each(function() {
+
+            var $this = $(this);
+
+            $this.on('click', function(e) {
+                e.preventDefault();
+                callback();
+            });
+
+            $this.on('keyup', function(e) {
+                if (e.keyCode === 13) {
+                    e.preventDefault();
+                    callback();
+                }
+            });
+        });
+    };
+
+    // Recursively removes the last empty element (img, audio, etc) or word in an element
+    $.fn.removeLastWord = function (chars) {
+
+        if ('undefined' === typeof chars) chars = 8;
+
+        return this.each(function () {
+
+            var $self = $(this);
+
+            if ($self.contents().length > 0) {
+                var $lastElement = $self.contents().last();
+                if ($lastElement[0].nodeType === 3) { // Text node
+                    var words = $lastElement.text().trim().split(' ');
+
+                    if (words.length > 1) {
+                        words.splice(words.length-1, 1);
+                        $lastElement[0].data = words.join(' '); // textnode.data
+                        return;
+                    } else if ('undefined' !== typeof chars && words.length === 1 && words[0].length > chars) {
+                        $lastElement[0].data = words.join(' ').substring(0, chars);
+                        return;
+                    }
+                }
+
+                $lastElement.removeLastWord(chars); // Element
+            } else {
+                // Empty element
+                $self.remove();
+            }
+        });
+    };
+
+    $.fn.swapClass = function (removeClass, addClass) {
+        return this.each(function () {
+            $(this).removeClass(removeClass).addClass(addClass);
+        });
+    };
+
+    $.fn.targetBlank = function () {
+        return this.each(function () {
+            $(this).find('a').prop('target', '_blank');
+        });
+    };
+
+    $.fn.toggleClass = function (class1, class2) {
+        return this.each(function () {
+            var $this = $(this);
+
+            if ($this.hasClass(class1)){
+                $(this).removeClass(class1).addClass(class2);
+            } else {
+                $(this).removeClass(class2).addClass(class1);
+            }
+
+        });
+    };
+
+    $.fn.toggleExpandText = function (chars, callback) {
+
+        return this.each(function () {
+
+            var $self = $(this);
+
+            var expandedText = $self.html();
+
+            if (chars > expandedText.length) return;
+
+            var expanded = false;
+
+            var collapsedText = expandedText.substr(0, chars);
+            collapsedText = collapsedText.substr(0, Math.min(collapsedText.length, collapsedText.lastIndexOf(" ")));
+
+            $self.toggle = function() {
+                $self.empty();
+
+                var $toggleButton = $('<a href="#" class="toggle"></a>');
+
+                if (expanded) {
+                    $self.html(expandedText + "&nbsp;");
+                    $toggleButton.text("less");
+                    $toggleButton.toggleClass("less", "more");
+                } else {
+                    $self.html(collapsedText + "&nbsp;");
                     $toggleButton.text("more");
                     $toggleButton.toggleClass("more", "less");
                 }
@@ -429,9 +498,32 @@
         });
     };
 
-    $.fn.horizontalMargins = function () {
-        var $self = $(this);
-        return parseInt($self.css('marginLeft')) + parseInt($self.css('marginRight'));
+    $.fn.toggleText = function (text1, text2) {
+        return this.each(function () {
+            var $this = $(this);
+
+            if ($this.text() === text1){
+                $(this).text(text2);
+            } else {
+                $(this).text(text1);
+            }
+
+        });
+    };
+
+    $.fn.updateAttr = function (attrName, oldVal, newVal) {
+
+        return this.each(function () {
+
+            var $this = $(this);
+
+            var attr = $this.attr(attrName);
+
+            if (attr && attr.indexOf(oldVal) === 0){
+                attr = attr.replace(oldVal, newVal);
+                $this.attr(attrName, attr);
+            }
+        });
     };
 
     $.fn.verticalMargins = function () {
@@ -439,88 +531,9 @@
         return parseInt($self.css('marginTop')) + parseInt($self.css('marginBottom'));
     };
 
-    $.fn.horizontalPadding = function () {
-        var $self = $(this);
-        return parseInt($self.css('paddingLeft')) + parseInt($self.css('paddingRight'));
-    };
-
     $.fn.verticalPadding = function () {
         var $self = $(this);
         return parseInt($self.css('paddingTop')) + parseInt($self.css('paddingBottom'));
-    };
-
-    $.fn.onPressed = function (callback) {
-
-        return this.each(function() {
-
-            var $this = $(this);
-
-            $this.on('click', function(e) {
-                e.preventDefault();
-                callback();
-            });
-
-            $this.on('keyup', function(e) {
-                if (e.keyCode === 13) {
-                    e.preventDefault();
-                    callback();
-                }
-            });
-        });
-    };
-
-    $.fn.onEnter = function (callback) {
-
-        return this.each(function() {
-
-            var $this = $(this);
-
-            $this.on('keyup', function(e) {
-                if (e.keyCode === 13) {
-                    e.preventDefault();
-                    callback();
-                }
-            });
-        });
-    };
-
-    var on = $.fn.on, timer;
-    $.fn.on = function () {
-        var args = Array.apply(null, arguments);
-        var last = args[args.length - 1];
-
-        if (isNaN(last) || (last === 1 && args.pop())) return on.apply(this, args);
-
-        var delay = args.pop();
-        var fn = args.pop();
-
-        args.push(function () {
-            var self = this, params = arguments;
-            clearTimeout(timer);
-            timer = setTimeout(function () {
-                fn.apply(self, params);
-            }, delay);
-        });
-
-        return on.apply(this, args);
-    };
-
-    $.mlp = {x:0,y:0}; // Mouse Last Position
-    function documentHandler(){
-        var $current = this === document ? $(this) : $(this).contents();
-        $current.mousemove(function(e){jQuery.mlp = {x:e.pageX,y:e.pageY}});
-        $current.find("iframe").load(documentHandler);
-    }
-    $(documentHandler);
-    $.fn.ismouseover = function(overThis) {
-        var result = false;
-        this.eq(0).each(function() {
-                var $current = $(this).is("iframe") ? $(this).contents().find("body") : $(this);
-                var offset = $current.offset();
-                result =    offset.left<=$.mlp.x && offset.left + $current.outerWidth() > $.mlp.x &&
-                offset.top<=$.mlp.y && offset.top + $current.outerHeight() > $.mlp.y;
-            });
-        return result;
     };
 
 })(jQuery);
