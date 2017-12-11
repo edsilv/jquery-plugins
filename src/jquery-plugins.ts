e03d384e56b85b37e6a1c814498a42a0ba321bd8
edsilv <e.silverton@gmail.com>
2017-12-11 12:59:59 +0000

    comment

--- src/jquery-plugins.ts
+++ src/jquery-plugins.ts
@@ -347,7 +347,7 @@
         });
     };
 
-    // todo: use built-in swapclass instead
+    // todo: use swapclass instead
     $.fn.switchClass = function (class1: string, class2: string) {
         return this.each(function () {
             const $this: JQuery = $(this);
